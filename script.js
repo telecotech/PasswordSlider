d1a56b0642b0d7d36e2f7cc7460cd68452204d4f
Mullens <telecotech82@gmail.com>
2023-02-07 21:53:00 -0500

    adding changes to my script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +10,15 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+
+
+
 generateBtn.addEventListener("click", writePassword);
