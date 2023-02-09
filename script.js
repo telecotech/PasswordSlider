8d54e42392ad3e30cfeb635c735af4f0ffc496f8
Mullens <telecotech82@gmail.com>
2023-02-08 20:38:15 -0500

    started over

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-document.getElementById("length").addEventListener("input", function() {
-  document.getElementById("charCount").innerHTML = "Password length: " + this.value + " characters";
-});
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-document.getElementById("generateBtn").addEventListener("click", function() {
-  var length = document.getElementById("length").value;
-  var includeUppercase = document.getElementById("uppercase").checked;
-  var includeLowercase = document.getElementById("lowercase").checked;
-  var includeNumbers = document.getElementById("numbers").checked;
-  var includeSymbols = document.getElementById("symbols").checked;
-  var password = generatePassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols);
-  document.getElementById("password").innerHTML = "Password: " + password;
-});
+  passwordText.value = password;
 
-function generatePassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
-  var password = "";
-  var characters = "";
-  if (includeUppercase) {
-    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-  if (includeLowercase) {
-    characters += "abcdefghijklmnopqrstuvwxyz";
-  }
-  if (includeNumbers) {
-    characters += "0123456789";
-  }
-  if (includeSymbols) {
-    characters += "!@#$%&?";
-  }
-  for (var i = 0; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return password;
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 
