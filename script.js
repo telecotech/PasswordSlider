d2b0e267a62e33f5ceea6528f9fe93b6d4f72bdc
Mullens <telecotech82@gmail.com>
2023-02-08 21:59:48 -0500

    had to bring my style sheet back in

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+
+var length = prompt("Choose a length of at least 8 characters and no more than 128 characters");
+console.log();
+var charactertypes = prompt("confirm whether or not to include lowercase, uppercase, numeric, and/or special characters");
+
+
+
+
+
+return;
+}
 
 // Write password to the #password input
 function writePassword() {
